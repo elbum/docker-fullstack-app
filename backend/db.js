1a3776950255155188d89194d1fcbc1c71fbef1d
elbum <elbumlee@gmail.com>
2021-02-02 14:10:06 +0900

    aws setup

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'test1234',
-    database: 'myapp'
+    // connectionLimit: 10,
+    // host: 'mysql',
+    // user: 'root',
+    // password: 'test1234',
+    // database: 'myapp'
+
+    // Docker environment 로 대체
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 
 })
 
